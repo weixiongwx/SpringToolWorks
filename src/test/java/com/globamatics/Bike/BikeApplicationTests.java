fbbe2b3d8e5d719c033647bbbd2b887d1fc489dd
shionchoo <chooweixiong@hotmail.com>
2021-08-10 03:20:40 +0800

    SpringBootCommit

---
+++ src/test/java/com/globamatics/Bike/BikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globamatics.Bike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
